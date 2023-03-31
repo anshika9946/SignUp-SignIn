7ca2ee2c7a02ca1810170a6803585cb1610cf770
Anshika <anshika2011087@akgec.ac.in>
2023-03-31 17:43:47 +0530

    setting routers

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const homeRouter = require('./routers/homeRouter')
 
 const port = process.env.port || 8080;
 
 const app =express();
 
-app.get('/',(err,res)=>{
-    res.send("hello")
-})
+app.set('view engine','ejs')
+
+app.use(express.static('public'))
+
+//for bodyParser
+app.use(bodyParser.urlencoded({extended:false}))
+
+app.use(bodyParser.json())
+
+//calling 
+app.use('/',homeRouter)
 
 app.listen(port)
